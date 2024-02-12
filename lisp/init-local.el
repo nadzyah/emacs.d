794ffe671d7766a5913b03d73feb768fa76e1a9a
nadzyah <nadzya.info@gmail.com>
2024-02-12 18:59:03 +0200

    Add c# support

--- lisp/init-local.el
+++ lisp/init-local.el
@@ -15,17 +15,24 @@
 
 ;(dired-sidebar-show-sidebar)
 
+(use-package csharp-mode
+  :mode "\\.cs$"
+  :hook
+  (csharp-mode-hook . subword-mode))
+
 (use-package tree-sitter :ensure t)
 (use-package tree-sitter-langs :ensure t)
 (use-package tree-sitter-indent :ensure t)
 
-(require-package 'auto-complete)
-(ac-config-default)
-(setq-default ac-sources
-          '(
-        ac-source-filename
-        ac-source-abbrev 
-        ac-source-dictionary
-        ac-source-words-in-same-mode-buffers))
+;(require-package 'auto-complete)
+;(ac-config-default)
+;(setq-default ac-sources
+;          '(
+;        ac-source-filename
+;        ac-source-abbrev 
+;        ac-source-dictionary
+;        ac-source-words-in-same-mode-buffers))
+
+(use-package auto-compelete :disabled)
 
 (provide 'init-local)
